a41b8cebc2dddd6c9df84106f7b799e0fec6d31a
Marcus Katalenas <katalenasm2@mailbx.winthrop.edu>
2020-06-30 13:49:24 -0400

    Added comments on slide 24 and slide 26 to attempt to seperate the pushes on the slides

--- js/slide24.js
+++ js/slide24.js
@@ -1,33 +1,33 @@
-var myData = [];
-var initalString = "";
-var arrayLength = 10;
+// var myData = [];
+// var initalString = "";
+// var arrayLength = 10;
 
-for(var index = 0; index < arrayLength; index++){
-    myData[index] = 1;
-    var nextString = new String(myData[index]);
-    initalString += nextString + " ";
-}
-console.log(initalString);
+// for(var index = 0; index < arrayLength; index++){
+//     myData[index] = 1;
+//     var nextString = new String(myData[index]);
+//     initalString += nextString + " ";
+// }
+// console.log(initalString);
 
-var segFault = false;
-var indexInput;
-var newNumberInput;
-var arrayString;
-while(!segFault){
-    arrayString = "";
-     indexInput = prompt("Enter an intger between 0-9");
+// var segFault = false;
+// var indexInput;
+// var newNumberInput;
+// var arrayString;
+// while(!segFault){
+//     arrayString = "";
+//      indexInput = prompt("Enter an intger between 0-9");
     
-    if(indexInput >= 0 && indexInput < myData.length){
-        newNumberInput = prompt("Enter the new number");
-        myData[indexInput] = newNumberInput;
+//     if(indexInput >= 0 && indexInput < myData.length){
+//         newNumberInput = prompt("Enter the new number");
+//         myData[indexInput] = newNumberInput;
 
-        for(var index = 0; index < myData.length; index++){
-            arrayString += myData[index] + " ";
-        }
-        console.log(arrayString);
-    }
-    else{
-        segFault = true;
-        console.log("EXIT");
-    }
-}
+//         for(var index = 0; index < myData.length; index++){
+//             arrayString += myData[index] + " ";
+//         }
+//         console.log(arrayString);
+//     }
+//     else{
+//         segFault = true;
+//         console.log("EXIT");
+//     }
+// }
--- js/slide26.js
+++ js/slide26.js
@@ -1,10 +1,10 @@
-var fibArray = [];
-fibArray[0] = 0;
-fibArray[1] = 1;
+// var fibArray = [];
+// fibArray[0] = 0;
+// fibArray[1] = 1;
 
-console.log(fibArray[0]);
-console.log(fibArray[1]);
-for(var nextIndex = 2; nextIndex < 60; nextIndex++){
-    fibArray[nextIndex] = fibArray[nextIndex - 2] + fibArray[nextIndex - 1];
-    console.log(fibArray[nextIndex]);
-}
+// console.log(fibArray[0]);
+// console.log(fibArray[1]);
+// for(var nextIndex = 2; nextIndex < 60; nextIndex++){
+//     fibArray[nextIndex] = fibArray[nextIndex - 2] + fibArray[nextIndex - 1];
+//     console.log(fibArray[nextIndex]);
+// }
